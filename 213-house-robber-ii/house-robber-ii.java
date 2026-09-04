9a8dfed81c65a315590d6e41c2463f964f5512a3
piyush7gautam <piyushkumargautam06@gmail.com>
2026-09-04 22:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (78.00%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    static int fun(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int take = nums[i] + prev2;
+            int skip = prev1;
+
+            int curr = Math.max(take, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        int case1 = fun(nums, 0, n - 2);
+        int case2 = fun(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+}
